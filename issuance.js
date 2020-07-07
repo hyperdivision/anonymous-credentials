c397f48c2225f43e23d214fbabdb47c2c6fef541
Christophe Diederichs <chris.d.itunes@gmail.com>
2020-07-07 12:13:43 +0200

    refactor to IssuingProtocol and Issuer entity

---
+++ issuance.js
@@ -0,0 +1,63 @@
+const assert = require('nanoassert')
+const curve = require('./curve')
+const schnorr = require('./schnorr-proof')
+
+const rand = curve.randomScalar
+const G1 = curve.G1
+
+module.exports = IssuingProtocol
+
+function IssuingProtocol (keys, attr) {
+  if (!(this instanceof IssuingProtocol)) return new IssuingProtocol(keys, attr)
+
+  const K_ = curve.randomPointG1()
+  const S_ = G1.mulScalar(K_, keys.sk.a)
+  const S0_ = G1.mulScalar(K_, keys.sk._a[0])
+
+  const setup = {
+    S_,
+    K_,
+    S0_
+  }
+
+  const response = respond(keys, attr, S_)
+
+  return {
+    setup,
+    attr,
+    response
+  }
+}
+
+function respond (keys, attr, S_) {
+  return (res) => {
+    const prover = schnorr([res.S, res.S0])
+    assert(prover.verify(res.R, res.proof), 'commitment to R fails validation.')
+
+    const inv_a = curve.F.inv(keys.sk.a)
+    const K = G1.mulScalar(res.S, inv_a)
+
+    const inv_a0 = curve.F.inv(keys.sk._a[0])
+    const S0_inv_a0 = G1.mulScalar(res.S0, inv_a0)
+
+    assert(!G1.eq(res.S, S_), 'S and S_ should be distinct.')
+    assert(G1.eq(K, S0_inv_a0), 'K and S0_inv_a0 should be equal.')
+
+    const kappa2 = rand()
+
+    const _S = attr.map((_, i) => G1.mulScalar(K, keys.sk._a[i + 1]))
+
+    const SKappa2 = G1.mulScalar(res.S, kappa2)
+    const C = _S.reduce((acc, el, i) => G1.add(acc, G1.mulScalar(el, attr[i])),
+      G1.add(res.R, G1.add(K, SKappa2)))
+
+    const T = G1.mulScalar(C, keys.sk.z)
+
+    return {
+      kappa2,
+      K,
+      _S,
+      T
+    }
+  }
+}
--- issuer.js
+++ issuer.js
@@ -1,73 +1,74 @@
 const assert = require('nanoassert')
-const curve = require('./curve')
-const keygen = require('./keygen')
-const schnorr = require('./schnorr-proof')
+const keys = require('./keygen')
+const IssuingProtocol = require('./issuance.js')
 
-const rand = curve.randomScalar
-const G1 = curve.G1
+module.exports = class {
+  constructor () {
+    this.certifications = {}
+    this.signingKeys = keys.signingKeys()
+    this.issuances = []
+  }
 
-module.exports = function (attr) {
-  const keys = keygen(attr.length + 1)
-  const cache = {}
-  
-  function one () {
-    const K_ = curve.randomPointG1()
-    const S_ = G1.mulScalar(K_, keys.sk.a)
-    const S0_ = G1.mulScalar(K_, keys.sk._a[0])
+  addIssuance (cert, attributes) {
+    const issuance = cert.issue(attributes)
+    this.issuances.push(issuance)
 
-    cache.S_ = S_
+    return issuance
+  }
+
+  grantCredential (res) {
+    const issuance = this.issuances.find(i => i.tag === res.tag)
+
+    const credential = issuance.response(res)
+
+    // assertion will throw on bad input before we execute following code
+    const identity = keys.userIds(this.signingKeys.sk)
+
+    this.certification.credentials.push({
+      attr: issuance.attr,
+      root: identity.root
+    })
 
     return {
-      S_,
-      K_,
-      S0_
+      credential,
+      identity
     }
   }
 
-  function two (res) {
-    const prover = schnorr([res.S, res.S0])
-    assert(prover.verify(res.R, res.proof), 'commitment to R fails validation.')
+  newCertification (fields) {
+    const certKeys = keys.issuingKeys(fields.length + 1)
+    const blacklist = []
+    const credentials = []
 
-    const inv_a = curve.F.inv(keys.sk.a)
-    const K = G1.mulScalar(res.S, inv_a)
-
-    const inv_a0 = curve.F.inv(keys.sk._a[0])
-    const S0_inv_a0 = G1.mulScalar(res.S0, inv_a0)
-
-    assert(!G1.eq(res.S, cache.S_), 'S and S_ should be distinct.')
-    assert(G1.eq(K, S0_inv_a0), 'K and S0_inv_a0 should be equal.')
-
-    const kappa2 = rand()
-
-    const _S = attr.map((_, i) => G1.mulScalar(K, keys.sk._a[i + 1]))
-
-    const SKappa2 = G1.mulScalar(res.S, kappa2)
-    let C = _S.reduce((acc, el, i) => {
-      return G1.add(acc, G1.mulScalar(el, attr[i]))
-    }, G1.add(res.R, G1.add(K, SKappa2)))
-
-    const T = G1.mulScalar(C, keys.sk.z)
-
-    return {
-      kappa2,
-      K,
-      _S,
-      T
+    function getPk () {
+      return certKeys.pk
     }
+
+    function issue (attributes) {
+      assert(attributes.length === fields.length)
+      const issuance = new IssuingProtocol(keys, fields)
+
+      return issuance
+    }
+
+    const certification = {
+      getPk,
+      issue,
+      blacklist,
+      credentials
+    }
+
+    this.certifications.push(certification)
+
+    return certification
   }
 
-  function getPk () {
-    return keys.pk
-  }
+  revokeCredential (revokeId, pk) {
+    const cert = this.certifications.find(c => Buffer.compare(c.getPk(), pk) === 0)
 
-  function getSk () {
-    return keys.sk
-  }
+    const identity = cert.credentials.find(keys.findRoot(revokeId))
+    cert.blacklist.push(identity.root)
 
-  return {
-    one,
-    two,
-    getPk,
-    getSk
+    return identity
   }
 }
